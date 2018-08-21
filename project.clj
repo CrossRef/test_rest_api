4ec3f1ebf38506f15c101a5067b771396781bb4a
Esha Datta <edatta@crossref.org>
2018-08-21 09:47:46 -0400

    initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject test_rest_api "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main test_rest_api.core
+  :dependencies [[org.clojure/clojure "1.8.0"] [org.clojure/data.csv "0.1.4"] [bigml/sampling "3.2"] [http-kit "2.2.0"]])
