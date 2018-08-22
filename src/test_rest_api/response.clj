675fb26629ef3585bba3fd212be0998a0af46f63
Esha Datta <edatta@crossref.org>
2018-08-22 16:55:52 -0400

    continuing to add tests

--- src/test_rest_api/response.clj
+++ src/test_rest_api/response.clj
@@ -20,23 +20,43 @@
   [status]
   (= status-ok status))
 
+(defn compare-result-num
+  [stage-results prod-results]
+  (= stage-results prod-results))
+
 (defn cmp-results
   [stage prod]
-  (let [stage-body (convert-to-json stage)
-        prod-body  (convert-to-json prod)]
-         (prn (:status stage-body))
-         (prn (:status prod-body))))
+  )
 
+(defn cmp-instances
+  [stage prod]
+  (let [stage-body (convert-to-json stage)
+        prod-body  (convert-to-json prod)
+        stage-results-num (:total-results (:message stage-body))
+        prod-results-num (:total-results (:message prod-body))
+        stage-top-2-results (take 2 (:items (:message stage-body)))
+        prod-top-2-results (take 2 (:items (:message prod-body)))
+        result-num-match (compare-result-num stage-results-num prod-results-num)]
+
+        (if (not result-num-match)
+          (message (str "Total result number mismatch:," stage-results-num ","  prod-results-num)))
+          (assoc {} :prod prod-top-2-results :stage stage-top-2-results)))
+
+(defn status-code-check
+  [query staging-rsp prod-rsp]
+  (let [stage-status (:status staging-rsp)
+        prod-status  (:status prod-rsp)
+        equal-status (status-code stage-status prod-status)]
+  (if (and equal-status (ok? stage-status))
+      (cmp-instances (:body staging-rsp) (:body prod-rsp))
+      (message (str "Status code difference," stage-status "," prod-status ","query)))))
 
 (defn compare-response
   [query staging-rsp prod-rsp]
-  (let [stage-status (:status staging-rsp)
-        prod-status (:status prod-rsp)
-        equal-status (status-code stage-status prod-status)]
-  (when (and equal-status (ok? stage-status))
-      (cmp-results (:body staging-rsp) (:body prod-rsp)))
-  (when-not equal-status
-    (message (str "Status code difference," stage-status "," prod-status ","query)))))
+  (prn query)
+  (status-code-check query staging-rsp prod-rsp))
+
+
   ;;write out to a message holder))
 
   ;; if 200, check body
