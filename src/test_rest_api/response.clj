77a49cb7efb93c56037edf5f1a52dee9c7e2f84b
Esha Datta <edatta@crossref.org>
2018-08-30 17:19:13 +0100

    adding functionality to check for type of urls and the type of response to be expected

--- src/test_rest_api/response.clj
+++ src/test_rest_api/response.clj
@@ -2,7 +2,8 @@
   (:require [clojure.data.csv :as csv]
             [org.httpkit.client :as http]
             [clojure.java.io :as io]
-            [cheshire.core :as json]))
+            [cheshire.core :as json]
+            [clojure.set]))
 (def status-ok 200)
 (defn message
   [msg]
@@ -31,12 +32,33 @@
    {:type "exact match - 1 result, not exact match - another result" :score 3}
    {:type "exact match - 1 result, not exact match - another result" :score 2}])
 
-(defn compare-results
-  [results log]
-  (let [prod-2-results (take 2 (:items (:message (convert-to-json (:prod results)))))
-        stage-results (:items (:message (convert-to-json (:stage results))))]
-        (when-not (clojure.set/subset? (set prod-2-results) (set stage-results))
-           (log {:type "no-results-exist-error" :msg "Top 2 production results don't exist in first page of stage results" :query (:query results)}))))
+
+;(defn compare-results
+  ;[n log {prod :prod stage :stage}]))
+
+(defn api-results->doi-seq
+  [rsp-body]
+  (->> rsp-body convert-to-json :message :items (map :DOI)))
+
+(defn api-results
+  [rsp-body]
+  (->> rsp-body convert-to-json :message :items))
+
+
+(defn get-position
+  "return mapping of prod to staging index of top n results"
+  [n {prod :prod stage :stage}]
+  (let [prod-dois (->> prod api-results->doi-seq (take n))
+        stage-dois (-> stage api-results->doi-seq)
+        stage-prod-position (map #(.indexOf stage-dois %) prod-dois)]
+   (into {} (map-indexed vector stage-prod-position))))
+
+
+(defn top-n-intersection
+  [n {prod :prod stage :stage}]
+  (let [prod-dois (->> prod api-results->doi-seq (take n) set)
+        stage-dois (-> stage api-results->doi-seq set)]
+        (clojure.set/intersection prod-dois stage-dois)))
 
 (defn chk-json-keys
   [results log]
@@ -46,13 +68,6 @@
         prod-keys (into [] (keys (first (take 1 (:items (:message (first prod-body)))))))]
         (when-not (= stage-keys prod-keys)
           (log {:type "json-keys-mismatch" :stage stage-keys :prod prod-keys :query (:query results)}))))
-
-(defn get-top-prod-2-results
-  [stage prod]
-  (let [stage-body (convert-to-json stage)
-        prod-body  (convert-to-json prod)
-        prod-top-2-results (take 2 (:items (:message prod-body)))]
-        (assoc {} :prod prod-top-2-results :stage (:items (:message stage-body)))))
 
 
 (defn chk-result-totals
@@ -78,6 +93,39 @@
         (when-not (and equal-status? (ok? stage-status))
              (log {:type "status-error" :msg err-msg :stage stage-status :production prod-status :query (:query results)}))))
 
+(def available-resources
+  {:works :DOI :funders :id :members :id :licenses :URL :journals [:title :publisher]})
+
+(def ar-vector
+  (into [] (map name (keys available-resources))))
+
+
+(def version "v1")
+
+(defn split-query
+  [query]
+  (filter #(not (= % version)) (remove clojure.string/blank? (clojure.string/split query #"\/"))))
+
+(defn plural-query?
+  [query]
+  (let [q (into [] (split-query query))
+        last-route (.indexOf ar-vector (last q))]
+   (if (> last-route -1)
+      true
+      false)))
+
+(defn get-unique-id-type
+  [query]
+  (let [q (into [] (split-query query))
+        first-route (.indexOf ar-vector (first q))]
+   (if (> first-route -1)
+      ((keyword (first q)) available-resources)
+      false)))
+
+(defn compare-response2
+  [query staging-rsp prod-rsp]
+  (plural-query? query))
+
 (defn compare-response
   [query staging-rsp prod-rsp]
   (let [result-log (atom [])
@@ -86,11 +134,13 @@
                        #(cons item %)))]
         (status-code-check {:stage staging-rsp :prod prod-rsp :query query} log-f)
         (when (= (count (filter #(and (= (:type %) "status-error") (= (:query %) query)) @result-log)) 0)
-           (chk-result-totals {:stage (:body staging-rsp) :prod (:body prod-rsp) :query query} log-f)
-           (when (= (count (filter #(and (= (:type %) "result-total-error") (= (:query %) query)) @result-log)) 0)
-             (chk-json-keys {:stage (:body staging-rsp) :prod (:body prod-rsp) :query query} log-f)
-             (compare-results {:stage (:body staging-rsp) :prod (:body prod-rsp) :query query} log-f)))
-        @result-log))
+           (if (plural-query? query)
+              (chk-result-totals {:stage (:body staging-rsp) :prod (:body prod-rsp) :query query} log-f)
+              (when (= (count (filter #(and (= (:type %) "result-total-error") (= (:query %) query)) @result-log)) 0)
+                (chk-json-keys {:stage (:body staging-rsp) :prod (:body prod-rsp) :query query} log-f)
+                (top-n-intersection 2 {:stage (:body staging-rsp) :prod (:body prod-rsp)})
+                (get-position 2 {:stage (:body staging-rsp) :prod (:body prod-rsp)}))))))
+        ;@result-log))
 
 
   ;; heartbeat response is different -- look into that
@@ -101,3 +151,6 @@
   ;; if within one or 2 positions, match it
 
   ;;(json/parse-string (:body staging-rsp) true))
+
+  ;; why does subset behave so weirdly
+  ;; if I want to find position of something, is there a less painful way to do it
